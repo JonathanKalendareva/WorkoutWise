804804a8a68f99163d77459e0d8cf9d98a6057d9
Mashroor Ali <alimashroor1@gmail.com>
2023-03-16 12:30:31 -0400

    added the goat

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeViewText, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeViewText style={styles.container}>
       <Text>fitnesstracker</Text>
       <StatusBar style="auto" />
-    </View>
+      <Image />
+    </SafeViewText>
   );
 }
 
@@ -17,6 +18,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
 
-    
+
   },
 });
