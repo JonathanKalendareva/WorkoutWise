3d8560e940e72fa68a581615da82de6c37b1c428
Jonathan <Jonathankalendarev@gmail.com>
2023-03-16 13:55:24 -0400

    Button additions

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, Image, View, SafeAreaView,Button} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>fitnesstracker</Text>
-      <StatusBar style="auto" />
-      <Image source={require('./assets/goat.jpg')} />
+      <Text style={styles.title}>fitnesstracker</Text>
+      <Image
+        style={styles.homeImage}
+        source={require('./assets/goat.jpg')} />
+        <Button
+        title = 'Sign up'
+        style = {styles.signUp}></Button>
+        <Button
+        title = 'Login' 
+        style = {styles.loginButton}></Button>
     </SafeAreaView>
   );
 }
@@ -16,9 +23,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#552583',
     alignItems: 'center',
-    justifyContent: 'center',
-
   },
 
-  //hello my name is jacka
+  title: {
+    marginTop: 30,
+    color: '#FDB927',
+    textAlign: 'center',
+    fontSize: 45,
+    fontWeight: 'bold',
+  },
+
+  homeImage: {
+    width: 350,
+    height: 300,
+    marginTop: 100,
+  },
+  signUp: {
+    color : '#552583',
+  },
+  loginButton:{
+    color : '#FDB927',
+
+  }
 });
