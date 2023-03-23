c5bbb7c3552bd6fc7b867e892303e9a770c19ba7
Jonathan <Jonathankalendarev@gmail.com>
2023-03-23 12:35:01 -0400

    added folder for screens

--- App.js
+++ App.js
@@ -1,77 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, View, SafeAreaView, Alert, TouchableOpacity, Dimensions, PixelRatio } from 'react-native';
+import {NavigationContainer} from 'react-native';
+import LandingPage from './Screens/LandingPage';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    // title and home image
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>fitnesstracker™</Text>
-      <Image
-        style={styles.homeImage}
-        source={require('./assets/goat.jpg')} />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Home" component = {LandingPage} options = {{title: "Welcome"}}/>
 
-      <TouchableOpacity onPress={console.log(Dimensions.get('window').width)}>
-        <View style={styles.signUpContainer}>
-          <Text style={styles.signUpText}>sign up</Text>
-        </View>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={console.log(Dimensions.get('window').height)}>
-        <View style={styles.loginContainer}>
-          <Text style={styles.loginText}>login</Text>
-        </View>
-      </TouchableOpacity>
-    </SafeAreaView>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#552583',
-    alignItems: 'center',
-  },
-
-  title: {
-    marginTop: Dimensions.get('window').height / 31,
-    color: '#FDB927',
-    textAlign: 'center',
-    fontSize: 40 / PixelRatio.getFontScale(),
-    fontWeight: 'bold',
-  },
-
-  homeImage: {
-    width: 350,
-    height: 300,
-    marginTop: Dimensions.get('window').height / 9.26,
-  },
-
-  signUpContainer: {
-    backgroundColor: "#FDB927",
-    padding: 15,
-    alignItems: "center",
-    alignSelf: "center",
-    borderRadius: 10,
-    marginTop: Dimensions.get('window').height / 6
-  },
-
-  signUpText: {
-    fontSize: 25 / PixelRatio.getFontScale(),
-    color: "#552583",
-    fontWeight: "bold"
-  },
-
-  loginContainer: {
-    marginTop: 10,
-    backgroundColor: "#552583",
-    padding: 10,
-    bottom: 0
-  },
-
-  loginText: {
-    fontSize: 25 / PixelRatio.getFontScale(),
-    color: "#FDB927",
-    fontWeight: "bold"
-  }
-}
-);
+};
