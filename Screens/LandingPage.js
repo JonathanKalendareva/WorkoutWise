bea4b96be90cf5622b4a8fd25fa5e29e789a3f6f
Jonathan <Jonathankalendarev@gmail.com>
2023-03-23 12:35:27 -0400

    updates

---
+++ Screens/LandingPage.js
@@ -0,0 +1,79 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Image, View, SafeAreaView, Alert, TouchableOpacity, Dimensions, PixelRatio, NavigationContainer} from 'react-native';
+
+export default function LandingPage() {
+  return (
+    // title and home image
+    <NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>fitnesstracker™</Text>
+      <Image
+        style={styles.homeImage}
+        source={require('./assets/goat.jpg')} />
+
+      <TouchableOpacity onPress={console.log(Dimensions.get('window').width)}>
+        <View style={styles.signUpContainer}>
+          <Text style={styles.signUpText}>sign up</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={console.log(Dimensions.get('window').height)}>
+        <View style={styles.loginContainer}>
+          <Text style={styles.loginText}>login</Text>
+        </View>
+      </TouchableOpacity>
+    </SafeAreaView>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#552583',
+    alignItems: 'center',
+  },
+
+  title: {
+    marginTop: Dimensions.get('window').height / 31,
+    color: '#FDB927',
+    textAlign: 'center',
+    fontSize: 40 / PixelRatio.getFontScale(),
+    fontWeight: 'bold',
+  },
+
+  homeImage: {
+    width: 350,
+    height: 300,
+    marginTop: Dimensions.get('window').height / 9.26,
+  },
+
+  signUpContainer: {
+    backgroundColor: "#FDB927",
+    padding: 15,
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 10,
+    marginTop: Dimensions.get('window').height / 6
+  },
+
+  signUpText: {
+    fontSize: 25 / PixelRatio.getFontScale(),
+    color: "#552583",
+    fontWeight: "bold"
+  },
+
+  loginContainer: {
+    marginTop: 10,
+    backgroundColor: "#552583",
+    padding: 10,
+    bottom: 0
+  },
+
+  loginText: {
+    fontSize: 25 / PixelRatio.getFontScale(),
+    color: "#FDB927",
+    fontWeight: "bold"
+  }
+}
+);
