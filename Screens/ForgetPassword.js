dbbfe0a91cef6c7ebf1819e72fc8059ba3919e5d
Jonathan <Jonathankalendarev@gmail.com>
2023-05-27 12:28:28 -0400

    added ForgetPassword Screen

---
+++ Screens/ForgetPassword.js
@@ -0,0 +1,37 @@
+import CustomInput from '../src/components/customInput';
+import CustomButton from '../src/components/CustomButton';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Auth } from 'aws-amplify';
+
+export function ForgetPassword()
+{
+    const onSendPressed = async data =>
+    { 
+        await Auth.forgotPassword(data.username)
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+}
+}
+
+return(
+     <SafeAreaView>
+    <View style={{ marginTop: 40 }} />
+    <CustomInput
+                name="email"
+                placeholder={"Email"}
+                control={control}
+                rules={{
+                    required: 'Email is required',
+                    pattern: {
+                        value: /^\w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$/,
+                        message: 'Invalid email address'
+                    }
+                }}
+            />
+    <CustomButton
+                text="Send"
+                onPress={handleSubmit(onSendPressed)} />
+        
+    </SafeAreaView>
+    
+)
