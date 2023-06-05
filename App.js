5cf39959c7090ada007c332af65447a8557258a5
Mashroor Ali <alimashroor1@gmail.com>
2023-06-05 10:57:10 -0400

    added auto log in feature, bringing users to home page

--- App.js
+++ App.js
@@ -7,95 +7,56 @@ import { ConfirmationPage } from './Screens/ConfirmationPage';
 import { ForgetPassword } from './Screens/ForgetPassword';
 import { NewPasswordScreen } from './Screens/NewPasswordScreen';
 import { HomePage } from './Screens/HomePage';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import awsconfig from './src/aws-exports.js'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, View} from 'react-native';
 
 Amplify.configure(awsconfig);
 
 const Stack = createNativeStackNavigator();
-const Navigation = () => {
-
-  try{
-    const authUser = await AuthenticatorAssertionResponse.currentAuthenticatedUser({bypassCache: true});
-  }catch(e)
-  {
-    setUser(null);
-  }
-
-  setUser(authUser);
-
-  useEffect(() => {
-  checkUser();
-  } , [])
-  
-  if(user == undefined)
-  {}
-
 
 export default function App() {
+  const [user, setUser] = useState(undefined);
+
+  const checkUser = async () => {
+    try {
+      const authUser = await Auth.currentAuthenticatedUser({bypassCache: true});
+      setUser(authUser);
+    } catch(error){
+      setUser(null)
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  if (user === undefined) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       {/* main navigator with screen options */}
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#491F6F'
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      >
+      <Stack.Navigator screenOptions={{ headerStyle: {backgroundColor: '#491F6F'}, headerTintColor: '#fff', headerTitleStyle: {fontWeight: 'bold', }, }} >
+        {user ? (
+          <Stack.Screen name="HomePage" component={HomePage} options={{ title: 'HomePage', }} />
+        ) : (
+          <>
         {/* different screens */}
-        <Stack.Screen
-          name="LandingPage"
-          component={LandingPage}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignUpPage"
-          component={SignUpPage}
-          options={{
-            title: 'Sign Up',
-          }}
-        />
-        <Stack.Screen
-          name="LoginPage"
-          component={LoginPage}
-          options={{
-            title: 'Log In',
-          }}
-        />
-        <Stack.Screen
-          name="ConfirmationPage"
-          component={ConfirmationPage}
-          options={{
-            title: 'Confirmation Page',
-          }}
-        />
-        <Stack.Screen
-          name="ForgetPassword"
-          component={ForgetPassword}
-          options={{
-            title: 'Forget Password',
-          }}
-        />
-        <Stack.Screen
-          name="NewPasswordScreen"
-          component={NewPasswordScreen}
-          options={{
-            title: 'New Password',
-          }}
-        />
-        <Stack.Screen
-          name="HomePage"
-          component={HomePage}
-          options={{
-            title: 'HomePage',
-          }}
-        />
+        <Stack.Screen name="LandingPage" component={LandingPage} options={{ headerShown: false }} />
+        <Stack.Screen name="SignUpPage" component={SignUpPage} options={{ title: 'Sign Up', }} />
+        <Stack.Screen name="LoginPage" component={LoginPage} options={{ title: 'Log In', }} />
+        <Stack.Screen name="ConfirmationPage" component={ConfirmationPage} options={{ title: 'Confirmation Page', }} />
+        <Stack.Screen name="ForgetPassword" component={ForgetPassword} options={{ title: 'Forget Password', }} />
+        <Stack.Screen name="NewPasswordScreen" component={NewPasswordScreen} options={{ title: 'New Password', }} />
+          </>
+        )}
+
       </Stack.Navigator>
     </NavigationContainer>
   );
